b8155b68f07f5499af7bbaba38b76aa43bba9712
deboradum <pvwpvw@protonmail.com>
2025-02-07 20:02:43 +0100

    create plot scrtipt

---
+++ plot.py
@@ -0,0 +1,20 @@
+
+
+def plot_average_spymaster_num_cards():
+    return
+
+
+def plot_win_rates():
+    # Bar plot with part of the bars different colors / shades, depending on win by guess, assassin, etc
+    return
+
+
+def plot_death_rate_by_assassin():
+    return
+
+
+def plot_win_types():
+    return
+
+if __name__ == "__main__":
+    pass
